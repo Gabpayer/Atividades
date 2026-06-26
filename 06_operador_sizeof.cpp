ec9d8cb5ace354c3e0faa65c3604aacb7206a9f2
Gabpayer <103573994+Gabpayer@users.noreply.github.com>
2026-06-26 14:53:15 -0300

    Create 06_operador_sizeof.cpp

---
+++ 06_operador_sizeof.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    double b;
+    char c;
+    cout << "Tamanho de int: " << sizeof(a) << " bytes" << endl;
+    cout << "Tamanho de double: " << sizeof(b) << " bytes" << endl;
+    cout << "Tamanho de char: " << sizeof(c) << " byte" << endl;
+    return 0;
+}
