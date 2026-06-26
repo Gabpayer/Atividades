bc499c9de9318370b5288e9f3a1ae08b7681e628
Gabpayer <103573994+Gabpayer@users.noreply.github.com>
2026-06-26 14:45:20 -0300

    Create 02_entrada_saida.cpp

---
+++ 02_entrada_saida.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Digite um numero: " << endl;
+    cin >> a;
+    cout << "Você digitou: " << a << endl;
+    return 0;
+}
