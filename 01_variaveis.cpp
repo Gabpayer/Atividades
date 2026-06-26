8ce7f4d0718407d575448be22cb91fc45f452910
Gabpayer <103573994+Gabpayer@users.noreply.github.com>
2026-06-26 14:44:23 -0300

    Create 01_variaveis.cpp

---
+++ 01_variaveis.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 10;
+    float b = 3.14;
+    double c = 14555.41114117;
+    char d = '/';
+    string e = "joao";
+    bool f = true;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    cout << f << endl;
+
+    return 0;
+}
