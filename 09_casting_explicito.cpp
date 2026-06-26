e668a665db1d7fce54858e207a67b590f540f99b
Gabpayer <103573994+Gabpayer@users.noreply.github.com>
2026-06-26 14:56:38 -0300

    Create 09_casting_explicito.cpp

---
+++ 09_casting_explicito.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a = 3.7;
+    int b = (int)a + 10;
+    cout << "Resultado com casting explicito: " << b << endl;
+    return 0;
+}
